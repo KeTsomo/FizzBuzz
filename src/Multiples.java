967c2b8a99a9073d3033bbe6916a72a0d1186bfb
KT <127895735+KeTsomo@users.noreply.github.com>
2023-09-12 10:23:21 -0400

    Update Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,18 +2,7 @@ public class Multiples {
     public static void main(String[] args) {
 
         int amount = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean multipleof3 = i % 3 == 0;
-            boolean multipleof5 = i % 5 == 0;
-
-            if (multipleof3) {
-                amount++;
-            } else if (multipleof5) {
-                amount++;
-            }
-
-
-        }
+  
         return amount;
 
 
